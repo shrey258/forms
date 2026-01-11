c9f667b41d7c22b9011b94f984b7d868362225e9
Shreyansh Gupta <hardikshrey123@gmail.com>
2026-01-11 17:35:49 +0530

    Add glass effect UI and hide header in layout
    Updated RootLayout to hide the header by default. Enhanced the Index screen with animated and glass effect components, added navigation arrow icons, and improved overall styling for a modern UI.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,10 @@
-import { Text, View } from "react-native";
+import { GlassView } from "expo-glass-effect";
+import { ArrowLeft, ArrowRight } from "lucide-react-native";
+import { View } from "react-native";
+import Animated, { FadeInDown, FadeOutUp } from "react-native-reanimated";
+
+
+
 
 export default function Index() {
   return (
@@ -7,9 +13,75 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "black",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Animated.View
+        entering={FadeInDown.springify()}
+        exiting={FadeOutUp.springify()}
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 10,
+          padding: 10,
+        }}
+      >
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 50,
+            backgroundColor: "red",
+          }}
+        />
+        <View
+          style={{
+            flex: 1,
+            height: 50,
+            borderRadius: 10,
+            overflow:"hidden"
+          }}
+        >
+          <GlassView style={{ flex: 1 }} />
+        </View>
+      </Animated.View>
+
+      <View
+        style={{
+          flex: -1,
+          flexDirection: "row",
+          gap: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 40,
+        }}
+      >
+        <View style={{ overflow: "hidden", borderRadius: 30 }}>
+          <GlassView
+            style={{
+              width: 60,
+              height: 60,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <ArrowLeft color="white" size={24} />
+          </GlassView>
+        </View>
+        <View style={{ overflow: "hidden", borderRadius: 30 }}>
+          <GlassView
+            style={{
+              width: 60,
+              height: 60,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <ArrowRight color="white" size={24} />
+          </GlassView>
+        </View>
+      </View>
     </View>
   );
 }
